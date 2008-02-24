c1b2aff1a6133540736a8432f0ac3eef27e18234
Giuseppe Rota <grota@users.sourceforge.net>
2008-02-24 19:06:12 +0000

    added #defines for exp2f and log2f for freebds (removed math_private.h and s_exp2f.c files)
    git-svn-id: https://qtpfsgui.svn.sourceforge.net/svnroot/qtpfsgui/trunk@272 479317e0-7f23-0410-9f0e-fdaa22894045

---
+++ src/arch/freebsd/math.h
@@ -0,0 +1,38 @@
+/**
+ * This file is a part of Qtpfsgui package.
+ * ---------------------------------------------------------------------- 
+ * Copyright (C) 2006,2007,2008 Giuseppe Rota
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * ---------------------------------------------------------------------- 
+ *
+ * @author Giuseppe Rota <grota@users.sourceforge.net>
+ */
+
+#ifndef _FREEBSDMATH_H
+#define _FREEBSDMATH_H
+
+#if defined(__FreeBSD__)
+
+#if __FreeBSD__ < 6
+#define exp2f(x)       ( (float)( exp2(x) ) )
+#endif
+
+#define log2(x)        (log(x)  / M_LN2)
+#define log2f(x)       (logf(x) / M_LN2)
+
+#endif
+
+#endif
