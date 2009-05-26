34203d3c67e87622850a4db50c582ebfefa492ad
Franco Comida <fcomida@users.sourceforge.net>
2009-05-26 18:30:30 +0000

    Implements a Dialog to set LDR image quality on saving
    git-svn-id: https://qtpfsgui.svn.sourceforge.net/svnroot/qtpfsgui/trunk@397 479317e0-7f23-0410-9f0e-fdaa22894045

---
+++ src/Common/imageQualityDialog.cpp
@@ -0,0 +1,57 @@
+/**
+ * This file is a part of Qtpfsgui package.
+ * ----------------------------------------------------------------------
+ * Copyright (C) 2006,2007,2008 Giuseppe Rota
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * ----------------------------------------------------------------------
+ *
+ * @author Giuseppe Rota <grota@users.sourceforge.net>
+ */
+#include <QImage>
+#include <QString>
+#include <QByteArray>
+#include <QBuffer>
+
+#include "imageQualityDialog.h"
+
+ImageQualityDialog::~ImageQualityDialog() {}
+
+ImageQualityDialog::ImageQualityDialog(QImage *img, QString fmt, QWidget *parent) : QDialog(parent) {
+        setupUi(this);
+	image = img;
+	format = fmt;
+	connect(getSizeButton, SIGNAL(clicked(void)), this, SLOT(calcFileSize(void)));
+	connect(spinBox, SIGNAL(valueChanged(int)), this, SLOT(reset(int)));
+	connect(horizontalSlider, SIGNAL(valueChanged(int)), this, SLOT(reset(int)));
+	connect(okButton, SIGNAL(clicked(void)), this, SLOT(accept(void)));
+}
+
+int ImageQualityDialog::getQuality(void) {
+	return spinBox->value();
+}
+
+void ImageQualityDialog::calcFileSize(void) {
+	int quality = spinBox->value();
+        QByteArray ba;
+        QBuffer buffer(&ba);
+        buffer.open(QIODevice::WriteOnly);
+        image->save(&buffer, (const char *) format.toLatin1(), quality);
+	label_filesize->setText(QString::number( ba.size() )); //the JPG on disk differs by 374 more bytes
+}
+
+void ImageQualityDialog::reset(int) {
+	label_filesize->setText("Unknown");
+}
