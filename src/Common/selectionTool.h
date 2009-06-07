63a6d5483fd08647cb7b47b1a0a73ec31d0dccdb
Franco Comida <fcomida@users.sourceforge.net>
2009-06-07 14:21:12 +0000

    Changed SelectableLabel to a more generic SelectionTool can can be plugged to any widget to paint a selection rectangle on it. I use it either in SmartScrollArea and EditingTools where it is plugged to previewWidget
    The old selection code in previewWidget is still there for now.
    
    git-svn-id: https://qtpfsgui.svn.sourceforge.net/svnroot/qtpfsgui/trunk@437 479317e0-7f23-0410-9f0e-fdaa22894045

---
+++ src/Common/selectionTool.h
@@ -0,0 +1,58 @@
+/**
+ * This file is a part of Qtpfsgui package.
+ * ---------------------------------------------------------------------- 
+ * Copyright (C) 2009 Franco Comida
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * ---------------------------------------------------------------------- 
+ *
+ * @author Franco Comida <fcomida@users.sourceforge.net>
+ *
+ */
+
+#ifndef SELECTIONTOOL_H
+#define SELECTIONTOOL_H
+
+#include <QMouseEvent>
+#include <QPaintEvent>
+#include <QResizeEvent>
+#include <QRubberBand>
+
+class SelectionTool : public QWidget
+{
+Q_OBJECT
+public:
+	SelectionTool(QWidget *parent=0);
+	bool setON(bool isON);
+	QRect getSelectionRect();
+	void removeSelection();
+signals:
+        void selectionReady(bool);
+        void moved(QPoint diff);
+        void scroll(int x, int y, int w, int h);
+protected:
+        void mousePressEvent(QMouseEvent *e);
+        void mouseMoveEvent(QMouseEvent *e);
+        void mouseReleaseEvent(QMouseEvent *e);
+	void paintEvent(QPaintEvent *e);
+	bool eventFilter(QObject *obj, QEvent *event);
+private:
+        QWidget *m_parent;
+ 	QRect m_selectionRect;
+	QPoint m_mousePos, m_origin;
+        bool isSelectionReady;
+	enum { NOACTION, PANNING, START_SELECTING, SELECTING, MOVING, RESIZING_LEFT, RESIZING_RIGHT, RESIZING_TOP, RESIZING_BOTTOM, RESIZING_XY} m_action;
+};
+#endif
