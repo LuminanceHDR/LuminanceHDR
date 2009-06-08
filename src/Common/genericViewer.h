f193693572a77197e4fd22bd7785583959548de4
Franco Comida <fcomida@users.sourceforge.net>
2009-06-08 20:37:39 +0000

    Added GenericViewe base class of both HdrViewer and LdrViewer TODO: merge in GenericViewer common code of subclasses
    This allow showing HDR in TonemappingDialog
    
    git-svn-id: https://qtpfsgui.svn.sourceforge.net/svnroot/qtpfsgui/trunk@445 479317e0-7f23-0410-9f0e-fdaa22894045

---
+++ src/Common/genericViewer.h
@@ -0,0 +1,38 @@
+/**
+ * This file is a part of Qtpfsgui package.
+ * ----------------------------------------------------------------------
+ * Copyright (C) 2009 Franco Comida
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * ----------------------------------------------------------------------
+ *
+ * @author Franco Comida <fcomida@users.sourceforge.net>
+ */
+
+#ifndef GENERICVIEWER_H
+#define GENERICVIEWER_H
+
+#include <QWidget>
+
+class GenericViewer : public QWidget 
+{
+Q_OBJECT
+public:
+	GenericViewer(QWidget *parent = 0);
+	virtual bool getFittingWin() = 0;
+public slots:
+	virtual void fitToWindow(bool checked) = 0;
+};
+#endif
