c44969fddc0282e8aadaff876c8cc499b1fb8cc5
Alexandre Prokoudine <prokoudine@users.sourceforge.net>
2010-01-08 17:14:53 +0000

    Added -w option to dcraw default prefs so that camera's white balance would be used. When neither -w nor -a is used, results are often reported as incorrect.
    git-svn-id: https://qtpfsgui.svn.sourceforge.net/svnroot/qtpfsgui/trunk@698 479317e0-7f23-0410-9f0e-fdaa22894045

--- src/Common/options.cpp
+++ src/Common/options.cpp
@@ -62,7 +62,7 @@ void LuminanceOptions::loadFromQSettings() {
 
 	settings.beginGroup(GROUP_EXTERNALTOOLS);
 		if (!settings.contains(KEY_EXTERNAL_DCRAW_OPTIONS))
-			settings.setValue(KEY_EXTERNAL_DCRAW_OPTIONS, QStringList() << "-T" << "-4" << "-q" << "3");
+			settings.setValue(KEY_EXTERNAL_DCRAW_OPTIONS, QStringList() << "-T" << "-4" << "-q" << "3" << "-w");
 		dcraw_options=settings.value(KEY_EXTERNAL_DCRAW_OPTIONS).toStringList();
 		//bug 2001032, remove spurious default QString "-a aligned_" value set by ver 1.9.2
 		if (!settings.contains(KEY_EXTERNAL_AIS_OPTIONS) || settings.value(KEY_EXTERNAL_AIS_OPTIONS).toString()=="-a aligned_")
