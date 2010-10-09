c3b4c6195828279f2c15c720d1d71d6868e0cf99
Davide Anastasia <davideanastasia@users.sourceforge.net>
2010-10-09 00:20:00 +0000

    LuminanceHDR 2.0.1
    git-svn-id: https://qtpfsgui.svn.sourceforge.net/svnroot/qtpfsgui/trunk@732 479317e0-7f23-0410-9f0e-fdaa22894045

--- src/Common/config.h
+++ src/Common/config.h
@@ -24,7 +24,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define LUMINANCEVERSION "2.0.0"
+#define LUMINANCEVERSION "2.0.1"
 #define TMOSETTINGSVERSION "0.5"
 
 #define KEY_TOOLBAR_MODE "MainWindowToolbarVisualizationMode"
