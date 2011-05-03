222ab3f9808f99345d16557f236c765c1eb79c9a
Davide Anastasia <davideanastasia@users.sourceforge.net>
2011-05-03 20:56:12 +0000

    UMessageBox: Unified MessageBox removes visual incoherence in MessageBox on different platforms. Still not completed, will be used inside Luminance HDR to give a coherent UI visual experience
    git-svn-id: https://qtpfsgui.svn.sourceforge.net/svnroot/qtpfsgui/trunk@901 479317e0-7f23-0410-9f0e-fdaa22894045

---
+++ src/UI/UMessageBox.cpp
@@ -0,0 +1,62 @@
+/**
+ * This file is a part of Luminance HDR package.
+ * ----------------------------------------------------------------------
+ * Copyright (C) 2011 Davide Anastasia
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * ----------------------------------------------------------------------
+ *
+ * This file provides an unified style for all the QMessageBox in Luminance HDR
+ *
+ * Original Work
+ * @author Davide Anastasia <davideanastasia@users.sourceforge.net>
+ *
+ *
+ */
+
+#include "UI/UMessageBox.h"
+
+#define UMESSAGEBOX_WIDTH 450 // pixel
+
+UMessageBox::UMessageBox(QWidget *parent) : QMessageBox(parent)
+{
+    m_horizontalSpacer = new QSpacerItem(UMESSAGEBOX_WIDTH, 0, QSizePolicy::Minimum, QSizePolicy::Expanding);
+    m_layout = (QGridLayout*)this->layout();
+    m_layout->addItem(m_horizontalSpacer, m_layout->rowCount(), 0, 1, m_layout->columnCount());
+
+#ifdef Q_WS_MAC
+    this->setWindowModality(Qt::WindowModal); // In OS X, the QMessageBox is modal to the window
+#endif
+}
+
+UMessageBox::UMessageBox(const QString &title, const QString &text, Icon icon,
+             int button0, int button1, int button2,
+             QWidget *parent,
+             Qt::WindowFlags f) :
+QMessageBox(title, text, icon, button0, button1, button2, parent, f)
+{
+    m_horizontalSpacer = new QSpacerItem(UMESSAGEBOX_WIDTH, 0, QSizePolicy::Minimum, QSizePolicy::Expanding);
+    m_layout = (QGridLayout*)this->layout();
+    m_layout->addItem(m_horizontalSpacer, m_layout->rowCount(), 0, 1, m_layout->columnCount());
+
+#ifdef Q_WS_MAC
+    this->setWindowModality(Qt::WindowModal); // In OS X, the QMessageBox is modal to the window
+#endif
+}
+
+void UMessageBox::showEvent(QShowEvent *event)
+{
+  QMessageBox::showEvent(event);
+}
---
+++ src/UI/UMessageBox.h
@@ -0,0 +1,118 @@
+/**
+ * This file is a part of Luminance HDR package.
+ * ----------------------------------------------------------------------
+ * Copyright (C) 2011 Davide Anastasia
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * ----------------------------------------------------------------------
+ *
+ * This file provides an unified style for all the QMessageBox in Luminance HDR
+ *
+ * Original Work
+ * @author Davide Anastasia <davideanastasia@users.sourceforge.net>
+ *
+ */
+
+#ifndef __UNIVERSAL_MESSAGE_BOX__
+#define __UNIVERSAL_MESSAGE_BOX__
+
+#include <QMessageBox>
+#include <QString>
+#include <QEvent>
+#include <QSpacerItem>
+#include <QGridLayout>
+#include <QStringList>
+#include <QFile>
+#include <QTextStream>
+
+#include "ui_about.h"
+#include "Common/config.h"
+#include "Common/global.h"
+#include "Common/options.h"
+#include "Common/archs.h"
+
+class UMessageBox : public QMessageBox
+{
+private:
+    // workaround to set size
+    QSpacerItem* m_horizontalSpacer;
+    QGridLayout* m_layout;
+public:
+  explicit UMessageBox(QWidget *parent = 0);
+  
+  UMessageBox(const QString &title, const QString &text, Icon icon,
+               int button0, int button1, int button2,
+               QWidget *parent = 0,
+               Qt::WindowFlags f = Qt::Dialog | Qt::MSWindowsFixedSizeDialogHint);
+  
+  virtual void showEvent(QShowEvent *event);
+
+  static void about(QWidget* parent = 0)
+  {
+      QDialog *about = new QDialog(parent);
+      about->setAttribute(Qt::WA_DeleteOnClose);
+      Ui::AboutLuminance ui;
+      ui.setupUi(about);
+      ui.authorsBox->setOpenExternalLinks(true);
+      ui.thanksToBox->setOpenExternalLinks(true);
+      ui.GPLbox->setTextInteractionFlags(Qt::TextSelectableByMouse);
+      ui.label_version->setText(ui.label_version->text().append(QString(LUMINANCEVERSION)));
+
+      bool license_file_not_found=true;
+      QString docDir = QCoreApplication::applicationDirPath();
+      docDir.append("/../Resources");
+      QStringList paths = QStringList( BASEDIR "/share/doc/luminance") << BASEDIR "/share/luminance" << docDir << "/Applications/luminance.app/Contents/Resources" << "./";
+      foreach (QString path,paths)
+      {
+          QString fname(path+QString("/LICENSE"));
+#ifdef WIN32
+          fname+=".txt";
+#endif
+          if (QFile::exists(fname))
+          {
+              QFile file(fname);
+              //try opening it
+              if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+                  break;
+              QTextStream ts(&file);
+              ui.GPLbox->setAcceptRichText(false);
+              ui.GPLbox->setPlainText(ts.readAll());
+              license_file_not_found=false;
+              break;
+          }
+      }
+      if (license_file_not_found)
+      {
+          ui.GPLbox->setOpenExternalLinks(true);
+          ui.GPLbox->setTextInteractionFlags(Qt::TextBrowserInteraction);
+          ui.GPLbox->setHtml(tr("%1 License document not found, you can find it online: %2here%3","%2 and %3 are html tags").arg("<html>").arg("<a href=\"http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt\">").arg("</a></html>"));
+      }
+      about->show();
+  }
+
+  static int warning(QString title, QString description, QWidget* parent = 0)
+  {
+      UMessageBox WarningMsgBox(parent);
+      WarningMsgBox.setText(title);
+      WarningMsgBox.setInformativeText(description);
+      WarningMsgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+      WarningMsgBox.setDefaultButton(QMessageBox::No);
+      WarningMsgBox.setIcon(QMessageBox::Warning);
+
+      return WarningMsgBox.exec();
+  }
+};
+
+#endif
