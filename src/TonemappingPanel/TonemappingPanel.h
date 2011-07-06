01b24d6a3bf6f5fac88631d7ec776c298b1d55e7
Franco Comida <fcomida@users.sourceforge.net>
2011-07-06 08:50:10 +0000

    TonemappingWindow: Deleted TonemappingWindow.* - Created TonemappingPanel directory, moved other stuff to TonemappingPanel, updated CMakeLists.txt and project.pro
    git-svn-id: https://qtpfsgui.svn.sourceforge.net/svnroot/qtpfsgui/trunk@994 479317e0-7f23-0410-9f0e-fdaa22894045

---
+++ src/TonemappingPanel/TonemappingPanel.h
@@ -0,0 +1,132 @@
+/**
+ * This file is a part of Luminance HDR package.
+ * ----------------------------------------------------------------------
+ * Copyright (C) 2006,2007 Giuseppe Rota
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * ----------------------------------------------------------------------
+ *
+ * Original Work
+ * @author Giuseppe Rota <grota@users.sourceforge.net>
+ * Improvements, bugfixing
+ * @author Franco Comida <fcomida@users.sourceforge.net>
+ *
+ */
+
+#ifndef TONEMAPPINGWIDGET_H
+#define TONEMAPPINGWIDGET_H
+
+#include <QStatusBar>
+
+#include "Core/TonemappingOptions.h"
+#include "Common/options.h"
+#include "Common/Gang.h"
+#include "Libpfs/frame.h"
+#include "ui_TonemappingPanel.h"
+
+class TonemappingPanel : public QWidget, private Ui::TonemappingPanel
+{
+    Q_OBJECT
+
+protected:
+    Gang
+            //mantiuk06
+            *contrastfactorGang,
+            *saturationfactorGang,
+            *detailfactorGang,
+            // mantiuk08
+            *colorSaturationGang,
+            *contrastEnhancementGang,
+            *luminanceLevelGang,
+            // fattal02
+            *alphaGang,
+            *betaGang,
+            *saturation2Gang,
+            *noiseGang,
+            *oldFattalGang,
+            // ashikhmin02
+            *contrastGang,
+            *simpleGang,
+            *eq2Gang,
+            // drago03
+            *biasGang,
+            // durand02
+            *spatialGang,
+            *rangeGang,
+            *baseGang,
+            // pattanaik00
+            *multiplierGang,
+            *coneGang,
+            *rodGang,
+            *autoYGang,
+            *pattalocalGang,
+            // reinhard02
+            *keyGang,
+            *phiGang,
+            *range2Gang,
+            *lowerGang,
+            *upperGang,
+            *usescalesGang,
+            // reinhard05
+            *brightnessGang,
+            *chromaticGang,
+            *lightGang,
+            //
+            *pregammaGang;
+
+    TMOperator currentTmoOperator;
+    TonemappingOptions toneMappingOptions;
+    QVector<int> sizes;
+    void fillToneMappingOptions();
+    void setupUndo();
+    void fromGui2Txt(QString destination); //i.e. WRITE tmo settings to text file
+    QString recentPathLoadSaveTmoSettings, tmoSettingsFilename;
+    int out_ldr_cs;
+
+    float heightToWidthRatio;
+    bool adding_custom_size;
+
+protected Q_SLOTS:
+    void on_pregammadefault_clicked();
+    void on_defaultButton_clicked();
+    void on_applyButton_clicked();
+    void on_undoButton_clicked();
+    void on_redoButton_clicked();
+    void on_savesettingsbutton_clicked();
+    void on_loadsettingsbutton_clicked();
+    //APPLY tmo settings from text file
+    void fromTxt2Gui();
+    //user wants a custom size.
+    void on_addCustomSizeButton_clicked();
+    void fillCustomSizeComboBox();
+    void updateCurrentTmoOperator(int);
+    void updateUndoState();
+
+public:
+    TonemappingPanel(QWidget *parent = 0);
+    ~TonemappingPanel();
+    void setSizes(int, int);
+
+public Q_SLOTS:
+    void setEnabled(bool);
+    void updatedHDR(pfs::Frame*);
+    void updateTonemappingParams(TonemappingOptions *opts);
+
+signals:
+    void startTonemapping(TonemappingOptions*);
+
+};
+
+#endif
