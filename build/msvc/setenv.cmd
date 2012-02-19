d8f4dc5a782a9611bddf9ad0efde55e13b11c721
Daniel Kaneider <danielkaneider@users.sourceforge.net>
2012-02-19 11:38:21 +0100

    [msvc] fixed broken tiff support and updated libliff and libjpeg

--- build/msvc/setenv.cmd
+++ build/msvc/setenv.cmd
@@ -1,7 +1,7 @@
 @echo off
 SET CYGWIN_DIR=c:\cygwin
 SET TEMP_DIR=.downloaddir
-SET CMAKE_DIR=C:\Data\Programs\cmake-2.8.4-win32-x86
+SET CMAKE_DIR=C:\Data\Programs\cmake-2.8.7-win32-x86
 
 REM Options:  -------------------------------------------
 
