e9a14982baa8399f95d36ed04c6e2c4ad2acaab2
Daniel Kaneider <danielkaneider@users.sourceforge.net>
2012-03-31 10:45:31 +0200

    [msvc] adaptions for compilation/testing

--- src/MainGui/main.cpp
+++ src/MainGui/main.cpp
@@ -55,12 +55,40 @@ QStringList getCliFiles(const QStringList& arguments)
 }
 }
 
+
+inline void customMessageHandler(QtMsgType type, const char *msg)
+{
+	QString txt;
+	switch (type) {
+	case QtDebugMsg:
+		txt = QString("Debug: %1").arg(msg);
+		break;
+	case QtWarningMsg:
+		txt = QString("Warning: %1").arg(msg);
+	break;
+	case QtCriticalMsg:
+		txt = QString("Critical: %1").arg(msg);
+	break;
+	case QtFatalMsg:
+		txt = QString("Fatal: %1").arg(msg);
+		abort();
+	}
+
+	QFile outFile("debuglog.txt");
+	outFile.open(QIODevice::WriteOnly | QIODevice::Append);
+	QTextStream ts(&outFile);
+	ts << txt << endl;
+}
+
+
 int main( int argc, char ** argv )
 {
     Q_INIT_RESOURCE(icons);
     QApplication application( argc, argv );
 
 #ifdef WIN32
+    //qInstallMsgHandler(customMessageHandler);
+
     bool found_DLL = false;
     foreach (QString path, application.libraryPaths())
     {
