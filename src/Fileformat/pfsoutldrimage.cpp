be88a2797c42d3adf48073dbeddec680aadf2934
Davide Anastasia <davideanastasia@users.sourceforge.net>
2012-04-28 16:22:59 +0100

    [pfsoutldrimage] Comment out unused function

--- src/Fileformat/pfsoutldrimage.cpp
+++ src/Fileformat/pfsoutldrimage.cpp
@@ -41,12 +41,12 @@
 namespace
 {
 
-inline int clamp_to_8bits(const float& value)
-{
-    if (value <= 0.f) return 0;
-    if (value >= 1.f) return 255;
-    return (int)(value*255.f + 0.5f);
-}
+//inline int clamp_to_8bits(const float& value)
+//{
+//    if (value <= 0.f) return 0;
+//    if (value >= 1.f) return 255;
+//    return (int)(value*255.f + 0.5f);
+//}
 
 //! \note I pass value by value, so I can use it as a temporary variable inside the function
 //! I will let the compiler do the optimization that it likes
