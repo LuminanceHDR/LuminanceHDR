bcd629fd454178c58edf03129493d3e863d20948
Davide Anastasia <davide.anastasia@gmail.com>
2013-02-03 13:37:23 +0000

    [pfs] fusion: use template method pattern to localise the implementation of the debevec merging operator into a single function, independent on the input data

--- src/HdrCreation/debevec.cpp
+++ src/HdrCreation/debevec.cpp
@@ -130,145 +130,14 @@ void computeTrustRange(const float* w, int M, int& minM, int& maxM)
     }
 }
 
-
-// LDR version
-int debevec_applyResponse(pfs::Array2D& xj, pfs::Array2D& yj, pfs::Array2D& zj,
-                          const float* arrayofexptime,
-                          const float* Ir, const float* Ig, const float* Ib,
-                          const float* w, int M,
-                          const QList<QImage*>& list)
+template <typename InputDataAdapter>
+int debevecApplyResponseCore(pfs::Array2D& xj,  pfs::Array2D& yj, pfs::Array2D& zj,
+                             const float* arrayofexptime,
+                             const float* Ir, const float* Ig, const float* Ib,
+                             const float* w, int M,
+                             const InputDataAdapter& inputData)
 {
-#ifndef NDEBUG
-    std::cerr << "debevec fusion, LDR" << std::endl;
-#endif
-
-    assert(list.size() != 0);
-    assert(M == 256);
-
-    int N       = list.size();
-    int width   = xj.getCols();
-    int height  = xj.getRows();
-
-    // number of saturated pixels
-    int saturated_pixels = 0;
-    // compute trust area weights
-    int minM = 0;
-    int maxM = M-1;
-    computeTrustRange(w, M, minM, maxM);
-
-    //////////////////////LDR INPUT
-    // for all pixels
-    for ( int j=0 ; j<width*height ; ++j )
-    {
-        float sumR = 0.0f;
-        float sumG = 0.0f;
-        float sumB = 0.0f;
-
-        float divR = 0.0f;
-        float divG = 0.0f;
-        float divB = 0.0f;
-
-        float maxti = -1e6f;
-        float minti = +1e6f;
-
-        int index_for_whiteR = -1;
-        int index_for_whiteG = -1;
-        int index_for_whiteB = -1;
-
-        int index_for_blackR = -1;
-        int index_for_blackG = -1;
-        int index_for_blackB = -1;
-
-        // for all exposures
-        for ( int i = 0; i < N; i++ )
-        {
-            QRgb currentPixel = reinterpret_cast<const QRgb*>(list.at(i)->bits())[j];
-
-            // pick the 3 channel values
-            int mR = qRed( currentPixel );
-            int mG = qGreen( currentPixel );
-            int mB = qBlue( currentPixel );
-
-            float ti = arrayofexptime[i];
-
-            // if at least one of the color channel's values are in the bright
-            // "not-trusted zone" and we have min exposure time
-            if ( (mR > maxM || mG > maxM || mB > maxM) && (ti < minti) ) {
-                // update the indexes_for_whiteRGB, minti
-                index_for_whiteR = mR;
-                index_for_whiteG = mG;
-                index_for_whiteB = mB;
-                minti = ti;
-            }
-
-            // if at least one of the color channel's values are in the dim
-            // "not-trusted zone" and we have max exposure time
-            if ( (mR < minM || mG < minM || mB < minM) && (ti > maxti) ) {
-                // update the indexes_for_blackRGB, maxti
-                index_for_blackR = mR;
-                index_for_blackG = mG;
-                index_for_blackB = mB;
-                maxti = ti;
-            }
-
-            float w_average=(w[mR] + w[mG] + w[mB])/3.0f;
-            sumR += w_average * Ir[mR] / float(ti);
-            divR += w_average;
-            sumG += w_average * Ig[mG] / float(ti);
-            divG += w_average;
-            sumB += w_average * Ib[mB] / float(ti);
-            divB += w_average;
-
-        } // END for all the exposures
-
-        if ( divR == 0.0f || divG == 0.0f || divB == 0.0f ) {
-            saturated_pixels++;
-            if (maxti > -1e6f) {
-                sumR = Ir[index_for_blackR] / float(maxti);
-                sumG = Ig[index_for_blackG] / float(maxti);
-                sumB = Ib[index_for_blackB] / float(maxti);
-                divR = divG = divB = 1.0f;
-            }
-            if (minti < +1e6f) {
-                sumR = Ir[index_for_whiteR] / float(minti);
-                sumG = Ig[index_for_whiteG] / float(minti);
-                sumB = Ib[index_for_whiteB] / float(minti);
-                divR = divG = divB = 1.0f;
-            }
-        }
-
-        if ( divR != 0.0f && divG != 0.0f && divB != 0.0f ) {
-            xj(j) = sumR/divR;
-            yj(j) = sumG/divG;
-            zj(j) = sumB/divB;
-        } else {
-            // we shouldn't be here anyway...
-            xj(j) = 0.0f;
-            yj(j) = 0.0f;
-            zj(j) = 0.0f;
-        }
-    } // END for all pixels
-
-    return saturated_pixels;
-}
-
-// HDR version
-int debevec_applyResponse(pfs::Array2D& xj,  pfs::Array2D& yj, pfs::Array2D& zj,
-                          const float* arrayofexptime,
-                          const float* Ir, const float* Ig, const float* Ib,
-                          const float* w, int M,
-                          const Array2DList& listhdrR, const Array2DList& listhdrG, const Array2DList& listhdrB)
-{
-#ifndef NDEBUG
-    std::cerr << "debevec fusion, HDR" << std::endl;
-#endif
-
-    assert(listhdrR.size() != 0);
-    assert(listhdrB.size() == listhdrG.size());
-    assert(listhdrB.size() == listhdrR.size());
-    assert(M == (int)(1 << 16));
-
-    int N       = listhdrR.size();
+    int N       = inputData.numFrames();
     int width   = xj.getCols();
     int height  = xj.getRows();
 
@@ -303,9 +172,9 @@ int debevec_applyResponse(pfs::Array2D& xj,  pfs::Array2D& yj, pfs::Array2D& zj,
         for (int i = 0; i < N; ++i)
         {
             // pick the 3 channel values
-            int mR = static_cast<int>( (*(listhdrR[i]))(j) );
-            int mG = static_cast<int>( (*(listhdrG[i]))(j) );
-            int mB = static_cast<int>( (*(listhdrB[i]))(j) );
+            int mR = inputData.getRed(i, j);
+            int mG = inputData.getGreen(i, j);
+            int mB = inputData.getBlue(i, j);
             float ti = arrayofexptime[i];
 
             // if at least one of the color channel's values are in the bright
@@ -366,4 +235,102 @@ int debevec_applyResponse(pfs::Array2D& xj,  pfs::Array2D& yj, pfs::Array2D& zj,
     } // END for all pixels
 
     return saturated_pixels;
+}
+
+struct Array2DListAdapter
+{
+    Array2DListAdapter(const Array2DList& listhdrR, const Array2DList& listhdrG, const Array2DList& listhdrB)
+        : m_listhdrR(listhdrR)
+        , m_listhdrG(listhdrG)
+        , m_listhdrB(listhdrB)
+    {}
+
+    int numFrames() const {
+        return m_listhdrR.size();
+    }
+
+    int getRed(int frameIdx, int pixelIdx) const {
+        return static_cast<int>( (*(m_listhdrR[frameIdx]))(pixelIdx) );
+    }
+
+    int getGreen(int frameIdx, int pixelIdx) const {
+        return static_cast<int>( (*(m_listhdrG[frameIdx]))(pixelIdx) );
+    }
+
+    int getBlue(int frameIdx, int pixelIdx) const {
+        return static_cast<int>( (*(m_listhdrB[frameIdx]))(pixelIdx) );
+    }
+
+private:
+    const Array2DList& m_listhdrR;
+    const Array2DList& m_listhdrG;
+    const Array2DList& m_listhdrB;
+};
+
+struct QImageQListAdapter
+{
+    QImageQListAdapter(const QList<QImage*>& list)
+        : m_list(list)
+    {}
+
+    int numFrames() const {
+        return m_list.size();
+    }
+
+    int getRed(int frameIdx, int pixelIdx) const {
+        return qRed( reinterpret_cast<const QRgb*>(m_list.at(frameIdx)->bits())[pixelIdx] );
+    }
+
+    int getGreen(int frameIdx, int pixelIdx) const {
+        return qGreen( reinterpret_cast<const QRgb*>(m_list.at(frameIdx)->bits())[pixelIdx] );
+    }
+
+    int getBlue(int frameIdx, int pixelIdx) const {
+        return qBlue( reinterpret_cast<const QRgb*>(m_list.at(frameIdx)->bits())[pixelIdx] );
+    }
+
+private:
+    const QList<QImage*>& m_list;
+};
+
+// LDR version
+int debevec_applyResponse(pfs::Array2D& xj, pfs::Array2D& yj, pfs::Array2D& zj,
+                          const float* arrayofexptime,
+                          const float* Ir, const float* Ig, const float* Ib,
+                          const float* w, int M,
+                          const QList<QImage*>& list)
+{
+#ifndef NDEBUG
+    std::cerr << "debevec fusion, LDR" << std::endl;
+#endif
+
+    assert(list.size() != 0);
+    assert(M == 256);
+
+    return debevecApplyResponseCore(xj, yj, zj,
+                                    arrayofexptime, Ir, Ig, Ib,
+                                    w, M,
+                                    QImageQListAdapter(list));
+}
+
+// HDR version
+int debevec_applyResponse(pfs::Array2D& xj,  pfs::Array2D& yj, pfs::Array2D& zj,
+                          const float* arrayofexptime,
+                          const float* Ir, const float* Ig, const float* Ib,
+                          const float* w, int M,
+                          const Array2DList& listhdrR, const Array2DList& listhdrG, const Array2DList& listhdrB)
+{
+#ifndef NDEBUG
+    std::cerr << "debevec fusion, HDR" << std::endl;
+#endif
+
+    assert(listhdrR.size() != 0);
+    assert(listhdrB.size() == listhdrG.size());
+    assert(listhdrB.size() == listhdrR.size());
+    assert(M == (int)(1 << 16));
+
+    return debevecApplyResponseCore(xj, yj, zj,
+                                    arrayofexptime, Ir, Ig, Ib,
+                                    w, M,
+                                    Array2DListAdapter(listhdrR, listhdrG, listhdrB));
 }
