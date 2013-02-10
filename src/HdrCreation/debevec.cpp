dfddc7ddf268320de9d0008d4b8d5ff2e470bb19
Davide Anastasia <davide.anastasia@gmail.com>
2013-02-10 11:52:56 +0000

    openmp: fix for crash #2

--- src/HdrCreation/debevec.cpp
+++ src/HdrCreation/debevec.cpp
@@ -246,9 +246,9 @@ int debevecApplyResponseCore(pfs::Array2D& xj,  pfs::Array2D& yj, pfs::Array2D& 
     VectorInt inputBlue(N);
 
     // for all pixels
-//#pragma omp parallel for reduction(+:saturatedPixels) \
-    private(inputRed, inputGreen, inputBlue) \
-    shared(xj, yj, zj, arrayofexptime, Ir, Ig, Ib, w, M, minM, maxM, N)
+// #pragma omp parallel for reduction(+:saturatedPixels) \
+//    private(inputRed, inputGreen, inputBlue) \
+//    shared(xj, yj, zj, arrayofexptime, Ir, Ig, Ib, w, M, minM, maxM, N)
     for (int j = 0; j < width*height; ++j)
     {
         // accumulate pixel values
