59696857805b161a909d5458f2e66592039c4f27
Davide Anastasia <davide.anastasia@gmail.com>
2013-03-13 14:57:43 +0000

    [cmake] 1. Improvements C++11 support in CMake; 2. clean up CMake project structure

--- cmake/Internationalization.cmake
+++ cmake/Internationalization.cmake
@@ -1,9 +1,3 @@
-# Holds al the file that will be translated
-SET(FILES_TO_TRANSLATE )
-
-SET(LUMINANCE_MODULES_CLI )
-SET(LUMINANCE_MODULES_GUI )
-
 IF(APPLE)
 SET(I18NDIR QCoreApplication::applicationDirPath\(\)+"/../Resources/i18n")
 #ADD_DEFINITIONS(-DI18NDIR=QCoreApplication::applicationDirPath\(\)+"/../Resources/i18n")
@@ -17,19 +11,7 @@ ELSEIF(WIN32)
 SET(I18NDIR QCoreApplication::applicationDirPath\(\)+"/i18n")
 ENDIF()
 
-IF(UNIX)
-ADD_DEFINITIONS(-Wall -Wno-unknown-pragmas)
-ENDIF()
-
 OPTION(UPDATE_TRANSLATIONS "Update source translation translations/*.ts files (WARNING: make clean will delete the source .ts files! Danger!)")
 OPTION(LUPDATE_NOOBSOLETE "While doing an lupdate, remove obsolete entries.")
 
-IF(NOT CMAKE_BUILD_TYPE)
-SET(CMAKE_BUILD_TYPE "RelWithDebInfo" )
-ENDIF()
-
-# MESSAGE(${CMAKE_BUILD_TYPE})
-
-IF(${CMAKE_BUILD_TYPE} STREQUAL "Release" OR ${CMAKE_BUILD_TYPE} STREQUAL "MinSizeRel")
-ADD_DEFINITIONS("-DQT_NO_DEBUG_OUTPUT")
-ENDIF()
+# add a function here that creates the translation when necessary
