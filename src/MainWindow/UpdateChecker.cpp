476c5f8a3acb020c99d2975cae57ebea2e5863ee
Daniel Kaneider <danielkaneider@users.sf.net>
2013-04-02 09:38:11 +0200

    [UpdateChecker] UI polishing

--- src/MainWindow/UpdateChecker.cpp
+++ src/MainWindow/UpdateChecker.cpp
@@ -21,9 +21,10 @@
  */
 
 #include <QDebug>
-#include <QDesktopServices>
-#include <QNetworkRequest>
 #include <QDomDocument>
+#include <QDesktopServices>
+#include <QMessageBox>
+#include <QNetworkRequest>
 
 
 #include "Common/global.h"
@@ -77,20 +78,31 @@ void UpdateChecker::requestFinished(QNetworkReply* reply)
             {
                 QString version = nodeVersion.toElement().text();
                 latestUrl = nodeUrl.toElement().text();
+                
+                QString msgTitle = QString("Luminance HDR %1").arg(version);
+                QString msgContent = tr("A new release is ready for download!");
+                QWidget* widgetP = (QWidget*)parent();
+
+                UpdateAvailableInfo* info = new UpdateAvailableInfo();
+                info->version = version;
+                info->url = latestUrl;
+                emit updateAvailable(info);
+
                 if (QSystemTrayIcon::supportsMessages())
                 {
-                    QWidget* widgetP = (QWidget*)parent();
                     m_tray = new QSystemTrayIcon(widgetP->windowIcon(), this);
                     m_tray->setToolTip(widgetP->windowTitle());
                     m_tray->show();
                     connect(m_tray, SIGNAL(messageClicked()), this, SLOT(trayMessageClicked()));
                     connect(m_tray, SIGNAL(activated(QSystemTrayIcon::ActivationReason)), this, SLOT(trayMessageClicked()));
-                    m_tray->showMessage(tr("New Release: %1").arg(version), tr("A new release..."));
+
+                    m_tray->showMessage(msgTitle, msgContent + "\n" + tr("Click to download, or select Help->Update!"));
                 }
-                UpdateAvailableInfo* info = new UpdateAvailableInfo();
-                info->version = version;
-                info->url = latestUrl;
-                emit updateAvailable(info);
+                else
+                {
+                    QMessageBox::question(widgetP, msgTitle, msgContent + "\n\n" + tr("Do you want to open the webpage for download now?"),
+                        QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes);
+                }
             }        
         }
     }
