585b4499f9b646b8a8661b025570bc9b8636b757
Daniel Kaneider <danielkaneider@users.sf.net>
2013-11-10 19:13:04 +0100

    [msvc] updated libraries, re-enabled boost

--- build/msvc/setenv.cmd
+++ build/msvc/setenv.cmd
@@ -1,10 +1,10 @@
 @echo off
 SET CYGWIN_DIR=c:\cygwin
 SET TEMP_DIR=.downloaddir
-SET CMAKE_DIR=C:\Data\Programs\cmake-2.8.10.1-win32-x86
+SET CMAKE_DIR=C:\Data\Programs\cmake-2.8.12-win32-x86
 SET VISUAL_STUDIO_VC_REDIST=C:\Program Files (x86)\%VS_PROG_FILES%\VC\redist\%RawPlatform%
-SET QTDIR=C:\Data\Develop\Qt\install-Qt5.1.1-msvc2012-x64
-rem SET QTDIR=C:\Data\Develop\Qt\4.8.2-x64
+rem SET QTDIR=C:\Data\Develop\Qt\5.0.1-x64\qtbase
+SET QTDIR=C:\Data\Develop\Qt\install-Qt5.1.1-msvc2012-x64\5.1.1\msvc2012_64_opengl
 
 REM Options:  -------------------------------------------
 
@@ -20,4 +20,4 @@ REM Optional variables
 REM SET L_BOOST_DIR=C:\Data\Develop\libhdrStuff
 
 REM should the Luminance git repo be updated (defaulting to true for read-only git download)
-SET UPDATE_REPO_LUMINANCE=1
+SET UPDATE_REPO_LUMINANCE=0
