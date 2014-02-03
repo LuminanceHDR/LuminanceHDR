2698d0e519aa117c68efee805808811a1e860c95
Davide Anastasia <davide.anastasia@gmail.com>
2014-02-03 01:13:17 +0000

    Indenting

--- src/HdrCreation/responses.cpp
+++ src/HdrCreation/responses.cpp
@@ -157,126 +157,123 @@ void ResponseLog10::fillResponse(ResponseContainer& response)
 
 void dump_gnuplot( const char* filename, const float* array, int M )
 {
-  FILE* fp = fopen(filename, "w");
-  fprintf(fp, "# GNUPlot dump\n");
-  for( int i=0 ; i<M ; i++ )
-    fprintf(fp, "%4d %16.9f\n", i, array[i]);
-  fclose(fp);
-  std::cerr << "GNUPLOT: save data to " << filename << std::endl;
+    FILE* fp = fopen(filename, "w");
+    fprintf(fp, "# GNUPlot dump\n");
+    for( int i=0 ; i<M ; i++ )
+        fprintf(fp, "%4d %16.9f\n", i, array[i]);
+    fclose(fp);
+    std::cerr << "GNUPLOT: save data to " << filename << std::endl;
 }
 
 void dump_gnuplot( const char* filename, const float* array, int M, int counter )
 {
-  char fn[2048];
-  sprintf(fn, filename, counter);
-  dump_gnuplot(fn, array, M);
+    char fn[2048];
+    sprintf(fn, filename, counter);
+    dump_gnuplot(fn, array, M);
 }
 
 void responseSave( FILE* file, const float* Ir, const float* Ig, const float* Ib, int M)
 {
-  // response curve matrix header
-  fprintf(file, "# Camera response curve, channels Ir, Ig, Ib \n");
-  fprintf(file, "# data layout: camera output | log10(response Ir) | response Ir | log10(response Ig) | response Ig | log10(response Ib) | response Ib \n");
-  fprintf(file, "# type: matrix\n");
-  fprintf(file, "# rows: %d\n", M);
-  fprintf(file, "# columns: 7\n");
+    // response curve matrix header
+    fprintf(file, "# Camera response curve, channels Ir, Ig, Ib \n");
+    fprintf(file, "# data layout: camera output | log10(response Ir) | response Ir | log10(response Ig) | response Ig | log10(response Ib) | response Ib \n");
+    fprintf(file, "# type: matrix\n");
+    fprintf(file, "# rows: %d\n", M);
+    fprintf(file, "# columns: 7\n");
 
-  // save response
-  for( int m=0 ; m<M ; m++ ) {
-     float logR = Ir[m] == 0.0f ? -6.0f : log10f(Ir[m]);
-     float logG = Ig[m] == 0.0f ? -6.0f : log10f(Ig[m]);
-     float logB = Ib[m] == 0.0f ? -6.0f : log10f(Ib[m]);
-     fprintf(file, " %4d %15.9f %15.9f %15.9f %15.9f %15.9f %15.9f \n", m, logR, Ir[m], logG, Ig[m], logB, Ib[m]);
-  }
-  fprintf(file, "\n");
+    // save response
+    for( int m=0 ; m<M ; m++ ) {
+        float logR = Ir[m] == 0.0f ? -6.0f : log10f(Ir[m]);
+        float logG = Ig[m] == 0.0f ? -6.0f : log10f(Ig[m]);
+        float logB = Ib[m] == 0.0f ? -6.0f : log10f(Ib[m]);
+        fprintf(file, " %4d %15.9f %15.9f %15.9f %15.9f %15.9f %15.9f \n", m, logR, Ir[m], logG, Ig[m], logB, Ib[m]);
+    }
+    fprintf(file, "\n");
 }
-
 
 void weightsSave( FILE* file, const float* w, int M, const char* name)
 {
-  // weighting function matrix header
-  fprintf(file, "# Weighting function\n");
-  fprintf(file, "# data layout: weight | camera output\n");
-  fprintf(file, "# name: %s\n", name);
-  fprintf(file, "# type: matrix\n");
-  fprintf(file, "# rows: %d\n", M);
-  fprintf(file, "# columns: 2\n");
-  
-  // save weights
-  for( int m=0 ; m<M ; m++ )
-    fprintf(file, " %15.9f %4d\n", w[m], m);
-  
-  fprintf(file, "\n");
-}
+    // weighting function matrix header
+    fprintf(file, "# Weighting function\n");
+    fprintf(file, "# data layout: weight | camera output\n");
+    fprintf(file, "# name: %s\n", name);
+    fprintf(file, "# type: matrix\n");
+    fprintf(file, "# rows: %d\n", M);
+    fprintf(file, "# columns: 2\n");
 
+    // save weights
+    for( int m=0 ; m<M ; m++ )
+        fprintf(file, " %15.9f %4d\n", w[m], m);
+
+    fprintf(file, "\n");
+}
 
 bool responseLoad( FILE* file, float* Ir, float* Ig, float* Ib, int M)
 {
-  char line[1024];
-  int m=0,c=0;
-  
-  // parse response curve matrix header
-  while( fgets(line, 1024, file) )
-    if( sscanf(line, "# rows: %d\n", &m) == 1 )
-      break;
-  if( m!=M )
-  {
-    std::cerr << "response: number of input levels is different,"
-              << " M=" << M << " m=" << m << std::endl; 
-    return false;
-  }
-  while( fgets(line, 1024, file) )
-    if( sscanf(line, "# columns: %d\n", &c) == 1 )
-      break;
-  if( c!=7 )
-    return false;
-  
-  // read response
-  float ignoreR, ignoreG, ignoreB;
-  for( int i=0 ; i<M ; i++ )
-  {
-    float valR, valG, valB;
-    if( fscanf(file, " %d %f %f %f %f %f %f \n", &m, &ignoreR, &valR, &ignoreG, &valG, &ignoreB, &valB) !=7 )
-      return false;
-    if( m<0 || m>M )
-      std::cerr << "response: camera value out of range,"
-                << " m=" << m << std::endl;
-    else {
-      Ir[m] = valR;
-      Ig[m] = valG;
-      Ib[m] = valB;
-	}
-  }
-  
-  return true;
-}
+    char line[1024];
+    int m=0,c=0;
 
+    // parse response curve matrix header
+    while( fgets(line, 1024, file) )
+        if( sscanf(line, "# rows: %d\n", &m) == 1 )
+            break;
+    if( m!=M )
+    {
+        std::cerr << "response: number of input levels is different,"
+                  << " M=" << M << " m=" << m << std::endl;
+        return false;
+    }
+    while( fgets(line, 1024, file) )
+        if( sscanf(line, "# columns: %d\n", &c) == 1 )
+            break;
+    if( c!=7 )
+        return false;
+
+    // read response
+    float ignoreR, ignoreG, ignoreB;
+    for( int i=0 ; i<M ; i++ )
+    {
+        float valR, valG, valB;
+        if( fscanf(file, " %d %f %f %f %f %f %f \n", &m, &ignoreR, &valR, &ignoreG, &valG, &ignoreB, &valB) !=7 )
+            return false;
+        if( m<0 || m>M )
+            std::cerr << "response: camera value out of range,"
+                      << " m=" << m << std::endl;
+        else {
+            Ir[m] = valR;
+            Ig[m] = valG;
+            Ib[m] = valB;
+        }
+    }
+
+    return true;
+}
 
 bool weightsLoad( FILE* file, float* w, int M)
 {
-  char line[1024];
-  int m=0,c=0;
+    char line[1024];
+    int m=0,c=0;
 
-  // parse weighting function matrix header
-  while( fgets(line, 1024, file) )
-    if( sscanf(line, "# rows: %d\n", &m) == 1 )
-      break;
-  if( m!=M )
-  {
-    std::cerr << "response: number of input levels is different,"
-              << " M=" << M << " m=" << m << std::endl; 
-    return false;
-  }
-  while( fgets(line, 1024, file) )
-    if( sscanf(line, "# columns: %d\n", &c) == 1 )
-      break;
-  if( c!=2 )
-    return false;
-  
-  // read response 
-  for( int i=0 ; i<M ; i++ )
-    if( fscanf(file, " %f %d\n", &(w[i]), &m) !=2 )
-      return false;
-  
-  return true;
+    // parse weighting function matrix header
+    while( fgets(line, 1024, file) )
+        if( sscanf(line, "# rows: %d\n", &m) == 1 )
+            break;
+    if( m!=M )
+    {
+        std::cerr << "response: number of input levels is different,"
+                  << " M=" << M << " m=" << m << std::endl;
+        return false;
+    }
+    while( fgets(line, 1024, file) )
+        if( sscanf(line, "# columns: %d\n", &c) == 1 )
+            break;
+    if( c!=2 )
+        return false;
+
+    // read response
+    for( int i=0 ; i<M ; i++ )
+        if( fscanf(file, " %f %d\n", &(w[i]), &m) !=2 )
+            return false;
+
+    return true;
 }
