f61c058550e0fd19aa641ba3364c3cb6af22f25e
heckflosse <heckflosse67@gmx.de>
2019-01-16 00:58:45 +0100

    Improve validation of xtrans array

--- src/include/xtranshelper.h
+++ src/include/xtranshelper.h
@@ -23,7 +23,7 @@
 namespace librtprocess {
 
 inline int fc(const unsigned cfa[6][6], unsigned row,  unsigned col) {
-    return cfa[(row)%6][(col)%6];
+    return cfa[row % 6][col % 6];
 }
 
 inline bool isgreen(const  unsigned cfa[6][6], unsigned row,  unsigned col) {
@@ -31,27 +31,73 @@ inline bool isgreen(const  unsigned cfa[6][6], unsigned row,  unsigned col) {
 }
 
 inline bool validateXtransCfa(const  unsigned cfa[6][6]) {
-    // rough check for xtrans cfa correctness
+    // rough check for xtrans cfa correctness, to be completed
     int count[3] = {};
     for (int i = 0; i < 6; ++i) {
         for (int j = 0; j < 6; ++j) {
             ++count[cfa[i][j]];
         }
     }
+    // xtrans 6x6 grid always has 8 reds, 20 greens and 8 blues
     if(count[0] == 8 && count[2] == 8 && count[1] == 20) {
-        bool perLine = true;
+        bool perRoworColumn = true;
+        // for each row/column of xtrans 6x6 grid the following has to be valid
+        // 1) number of reds == number of blues
+        // 2) number of reds is either one or two
+        // 3) number of reds + number of greens + number of blues == 6
+        // First check rows
         for (int i = 0; i < 6; ++i) {
-            int countpl[3] = {};
+            int countpr[3] = {};
             for (int j = 0; j < 6; ++j) {
-                ++countpl[cfa[i][j]];
+                ++countpr[cfa[i][j]];
             }
-            if (countpl[0] != countpl[2] || countpl[0] < 1 || countpl[0] > 2 || (countpl[1] + 2* countpl[0] != 6)) {
-                perLine = false;
+            if (countpr[0] != countpr[2] || countpr[0] < 1 || countpr[0] > 2 || (countpr[1] + 2 * countpr[0] != 6)) {
+                perRoworColumn = false;
                 break;
             }
         }
-        if (perLine) {
-            return true;
+        if (perRoworColumn) {
+            // Rows are valid, check columns
+            for (int i = 0; i < 6; ++i) {
+                int countpc[3] = {};
+                for (int j = 0; j < 6; ++j) {
+                    ++countpc[cfa[j][i]];
+                }
+                if (countpc[0] != countpc[2] || countpc[0] < 1 || countpc[0] > 2 || (countpc[1] + 2 * countpc[0] != 6)) {
+                    perRoworColumn = false;
+                    break;
+                }
+            }
+            if (perRoworColumn) {
+                // Columns are also valid
+                // now check green neighborhood
+                bool checkGreens = true;
+                for (int i = 1; i < 5; ++i) {
+                    for (int j = 1; j < 5; ++j) {
+                        if (cfa[i][j] == 1) {
+                            // for each cross around a green pixel the following has to be valid
+                            // 1) number of reds == number blues
+                            // 2) number of reds is either one or two
+                            // 3) if number of reds is two, then right and left pixel of cross must have same color
+                            int count[3] = {};
+                            count[cfa[i - 1][j]]++;
+                            count[cfa[i + 1][j]]++;
+                            count[cfa[i][j - 1]]++;
+                            count[cfa[i][j + 1]]++;
+                            if (count[0] != count[2] || (count[0] != 1 && count[0] != 2) || (count[0] == 2 && cfa[i][j - 1] != cfa[i][j + 1])) {
+                                checkGreens = false;
+                                break;
+                            }
+                        }
+                    }
+                    if (!checkGreens) {
+                        break;
+                    }
+                }
+                if (checkGreens) {
+                    return true;
+                }
+            }
         }
     }
     
