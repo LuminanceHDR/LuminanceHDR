5889d38a0b977a331f65ea99bf6ae866ce3f95ad
heckflosse <heckflosse67@gmx.de>
2019-02-01 15:26:26 +0100

    Added hphd demosaic from RT

--- src/include/bayerhelper.h
+++ src/include/bayerhelper.h
@@ -58,4 +58,29 @@ inline bool validateBayerCfa(unsigned colors, const unsigned cfa[2][2]) {
 
     return false;
 }
+
+inline void interpolate_row_redblue (const float * const *rawData, const unsigned cfarray[2][2], float* ar, float* ab, const float * const pg, const float * const cg, const float * const ng, int i, int width)
+{
+    if (fc(cfarray, i, 0) == 2 || fc(cfarray, i, 1) == 2) {
+        std::swap(ar, ab);
+    }
+
+    // RGRGR or GRGRGR line
+    for (int j = 3; j < width - 3; ++j) {
+        if (!(fc(cfarray, i, j) & 1)) {
+            // keep original value
+            ar[j] = rawData[i][j];
+            // cross interpolation of red/blue
+            float rb = (rawData[i - 1][j - 1] - pg[j - 1] + rawData[i + 1][j - 1] - ng[j - 1]);
+            rb += (rawData[i - 1][j + 1] - pg[j + 1] + rawData[i + 1][j + 1] - ng[j + 1]);
+            ab[j] = cg[j] + rb * 0.25f;
+        } else {
+            // linear R/B-G interpolation horizontally
+            ar[j] = cg[j] + (rawData[i][j - 1] - cg[j - 1] + rawData[i][j + 1] - cg[j + 1]) / 2;
+            // linear B/R-G interpolation vertically
+            ab[j] = cg[j] + (rawData[i - 1][j] - pg[j] + rawData[i + 1][j] - ng[j]) / 2;
+        }
+    }
+}
+
 }
