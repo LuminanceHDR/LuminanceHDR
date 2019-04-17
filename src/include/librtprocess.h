dd0e857930954b19e14cb1ad2f08b500de664be0
CarVac <c.lo.to.da.down.lo@gmail.com>
2019-04-16 22:01:45 -0400

    initial highlight reconstruction commit

--- src/include/librtprocess.h
+++ src/include/librtprocess.h
@@ -38,5 +38,6 @@ rpError vng4_demosaic (int width, int height, const float * const *rawData, floa
 rpError igv_demosaic(int winw, int winh, const float * const *rawData, float **red, float **green, float **blue, const unsigned cfarray[2][2], const std::function<bool(double)> &setProgCancel);
 rpError lmmse_demosaic(int width, int height, const float * const *rawData, float **red, float **green, float **blue, const unsigned cfarray[2][2], const std::function<bool(double)> &setProgCancel, int iterations);
 rpError CA_correct(int winx, int winy, int winw, int winh, const bool autoCA, size_t autoIterations, const double cared, const double cablue, bool avoidColourshift, const float * const *rawDataIn, float **rawDataOut, const unsigned cfarray[2][2], const std::function<bool(double)> &setProgCancel, double fitParams[2][2][16], bool fitParamsIn, float inputScale = 65535.f, float outputScale = 65535.f);
+rpError HLRecovery_inpaint(const int width, const int height, float **red, float **green, float **blue)
 
 #endif
