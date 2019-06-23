e8ebd0419cc4fcba8a19021b26b645f87db3a6e8
heckflosse <heckflosse67@gmx.de>
2019-06-23 17:13:22 +0200

    Fix a warning in debug build

--- src/include/LUT.h
+++ src/include/LUT.h
@@ -61,13 +61,9 @@
 
 #include <cstring>
 #include <cstdint>
-#include <cassert>
-
 #ifndef NDEBUG
-#include <glibmm.h>
-#include <fstream>
+#include <cassert>
 #endif
-
 #include "opthelper.h"
 #include "rt_math.h"
 
@@ -480,26 +476,6 @@ public:
         T p2 = data[idx + 1] - p1;
         return (p1 + p2 * diff);
     }
-
-#ifndef NDEBUG
-    // Debug facility ; dump the content of the LUT in a file. No control of the filename is done
-    void dump(Glib::ustring fname)
-    {
-        if (size) {
-            Glib::ustring fname_ = fname + ".xyz"; // TopSolid'Design "plot" file format
-            std::ofstream f (fname_.c_str());
-            f << "$" << std::endl;
-
-            for (unsigned int iter = 0; iter < size; iter++) {
-                f << iter << ", " << data[iter] << ", 0." << std::endl;
-            }
-
-            f << "$" << std::endl;
-            f.close ();
-        }
-    }
-#endif
-
 
     operator bool (void) const
     {
