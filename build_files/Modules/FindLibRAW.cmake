8c9dccd69523933a6e4ff9c9312c66dcc4c1cc34
Franco Comida <fcomida@users.sourceforge.net>
2020-09-28 10:09:35 +0200

    fix CMake warnings

--- build_files/Modules/FindLibRAW.cmake
+++ build_files/Modules/FindLibRAW.cmake
@@ -1,34 +1,34 @@
 # - Find LibRAW library
 # Find the native LibRAW includes and library
 # This module defines
-#  LIBRAW_INCLUDE_DIR, where to find libraw.h, etc.
-#  LIBRAW_LIBRARIES, libraries to link against to use LibRAW.
-#  LIBRAW_FOUND, If false, do not try to use LibRAW.
+#  LibRAW_INCLUDE_DIR, where to find libraw.h, etc.
+#  LibRAW_LIBRARIES, libraries to link against to use LibRAW.
+#  LibRAW_FOUND, If false, do not try to use LibRAW.
 # also defined, but not for general use are
-#  LIBRAW_LIBRARY, where to find the LibRAW library.
+#  LibRAW_LIBRARY, where to find the LibRAW library.
 
-FIND_PATH(LIBRAW_INCLUDE_DIR libraw/libraw.h)
+FIND_PATH(LibRAW_INCLUDE_DIR libraw/libraw.h)
 
-SET(LIBRAW_NAMES ${LIBRAW_NAMES} raw_r)
-FIND_LIBRARY(LIBRAW_LIBRARY NAMES ${LIBRAW_NAMES} )
+SET(LibRAW_NAMES ${LibRAW_NAMES} raw_r)
+FIND_LIBRARY(LibRAW_LIBRARY NAMES ${LibRAW_NAMES} )
 
-# handle the QUIETLY and REQUIRED arguments and set LIBRAW_FOUND to TRUE if 
+# handle the QUIETLY and REQUIRED arguments and set LibRAW_FOUND to TRUE if
 # all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(LIBRAW  DEFAULT_MSG  LIBRAW_LIBRARY  LIBRAW_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibRAW  DEFAULT_MSG  LibRAW_LIBRARY  LibRAW_INCLUDE_DIR)
 
-IF(LIBRAW_FOUND)
-  SET( LIBRAW_LIBRARIES ${LIBRAW_LIBRARY} )
-  MESSAGE(STATUS "Found LibRAW (LIBRAW_INCLUDE_DIR = ${LIBRAW_INCLUDE_DIR})")
-  MESSAGE(STATUS "Found LibRAW (LIBRAW_LIBRARIES = ${LIBRAW_LIBRARIES})")
+IF(LibRAW_FOUND)
+  SET( LibRAW_LIBRARIES ${LibRAW_LIBRARY} )
+  MESSAGE(STATUS "Found LibRAW (LibRAW_INCLUDE_DIR = ${LibRAW_INCLUDE_DIR})")
+  MESSAGE(STATUS "Found LibRAW (LibRAW_LIBRARIES = ${LibRAW_LIBRARIES})")
   IF(UNIX)
     IF(APPLE)
       EXECUTE_PROCESS(COMMAND ${CMAKE_SOURCE_DIR}/build_files/platforms/macosx/find_demosaicing_gpl2.sh
-                    ${LIBRAW_LIBRARY} WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+                    ${LibRAW_LIBRARY} WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
                     OUTPUT_VARIABLE _output OUTPUT_STRIP_TRAILING_WHITESPACE)
     ELSE()
       EXECUTE_PROCESS(COMMAND ${CMAKE_SOURCE_DIR}/build_files/platforms/linux/find_demosaicing_gpl2.sh
-                    ${LIBRAW_LIBRARY} WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+                    ${LibRAW_LIBRARY} WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
                     OUTPUT_VARIABLE _output OUTPUT_STRIP_TRAILING_WHITESPACE)
     ENDIF()
     IF(_output)
@@ -40,11 +40,11 @@ IF(LIBRAW_FOUND)
 
     IF(APPLE)
       EXECUTE_PROCESS(COMMAND ${CMAKE_SOURCE_DIR}/build_files/platforms/macosx/find_demosaicing_gpl3.sh
-                    ${LIBRAW_LIBRARY} WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+                    ${LibRAW_LIBRARY} WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
                     OUTPUT_VARIABLE _output OUTPUT_STRIP_TRAILING_WHITESPACE)
     ELSE()
       EXECUTE_PROCESS(COMMAND ${CMAKE_SOURCE_DIR}/build_files/platforms/linux/find_demosaicing_gpl3.sh
-                    ${LIBRAW_LIBRARY} WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+                    ${LibRAW_LIBRARY} WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
                     OUTPUT_VARIABLE _output OUTPUT_STRIP_TRAILING_WHITESPACE)
     ENDIF()
     IF(_output)
@@ -54,8 +54,8 @@ IF(LIBRAW_FOUND)
       MESSAGE(STATUS "Demosaicing pack GPL3 not found")
     ENDIF(_output)
   ENDIF(UNIX)
-ELSE(LIBRAW_FOUND)
+ELSE(LibRAW_FOUND)
   MESSAGE(FATAL_ERROR "Could not find LibRAW")
-ENDIF(LIBRAW_FOUND)
+ENDIF(LibRAW_FOUND)
 
-MARK_AS_ADVANCED(LIBRAW_INCLUDE_DIR LIBRAW_LIBRARIES)
+MARK_AS_ADVANCED(LibRAW_INCLUDE_DIR LibRAW_LIBRARIES)
