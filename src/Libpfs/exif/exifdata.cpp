13ff9f3325e4bc03e2331656acb3d1376cdf84bd
Thomas Klausner <wiz@gatalith.at>
2023-07-19 10:04:23 +0200

    Fix build with exiv2 0.28.

--- src/Libpfs/exif/exifdata.cpp
+++ src/Libpfs/exif/exifdata.cpp
@@ -57,7 +57,7 @@ void ExifData::fromFile(const std::string &filename) {
       return;
     }
     try {
-        ::Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open(filename);
+        ::Exiv2::Image::UniquePtr image = Exiv2::ImageFactory::open(filename);
         image->readMetadata();
         ::Exiv2::ExifData &exifData = image->exifData();
 
@@ -129,7 +129,7 @@ MissingFNumber:
          */
         if ((it = exifData.findKey(Exiv2::ExifKey("Exif.Image.Orientation"))) !=
             exifData.end()) {
-            long rotation = it->toLong();
+            long rotation = it->toInt64();
             switch (rotation) {
                 case 3:
                     m_orientation = 180;
@@ -142,7 +142,7 @@ MissingFNumber:
                     break;
             }
         }
-    } catch (Exiv2::AnyError &e) {
+    } catch (Exiv2::Error &e) {
         return;
     }
 }
